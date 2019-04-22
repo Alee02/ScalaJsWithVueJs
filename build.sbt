cbbcd62abf05d5f267247f84dbb3198fe31c944f
Alee02 <a-shah@hotmail.co.uk>
2019-04-22 14:25:31 +0100

    @

---
+++ build.sbt
@@ -0,0 +1,9 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "ScalaJsWithVueJs"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+scalaJSUseMainModuleInitializer := true
---
+++ src/main/scala/hello/HelloScalaJs.scala
@@ -0,0 +1,9 @@
+package hello
+
+import scala.scalajs.js.JSApp
+
+object HelloScalaJs extends JSApp{
+  override def main(): Unit = {
+    println("Hello Scala.js")
+  }
+}
